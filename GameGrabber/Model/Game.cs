2c8d5e8f9be765b0dcafe8cfb9b5b0f97ec2af8e
Lucas-Kinoo <lucas.kinoo@student.howest.be>
2023-04-11 18:31:17 +0200

    Cleanup game titles: remove store name and 'givaway'

--- GameGrabber/Model/Game.cs
+++ GameGrabber/Model/Game.cs
@@ -1,13 +1,24 @@
 ﻿using Newtonsoft.Json;
+using System.Linq;
 
 namespace GameGrabber.Model
 {
     internal class Game
     {
+        public string Title
+        {
+            get
+            {
+                // Most titles are int the format "Title (Store) givaway"
+                // We only want the title, so we remove the rest
+                return TitleRaw?.Split('(')?.First()?.Trim() ?? TitleRaw;
+            }
+        }
+
         [JsonProperty(PropertyName = "title")]
-        public string Title { get; set; }
+        private string TitleRaw { get; set; }
 
         [JsonProperty(PropertyName = "image")]
-        public string ImageUrl { get; set; }
+        public string ImageUrl { get; }
     }
 }
