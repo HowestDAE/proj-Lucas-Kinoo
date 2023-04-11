ae33903b93e3c287e62f73d1890a935ad058d8f3
Lucas-Kinoo <lucas.kinoo@student.howest.be>
2023-04-11 18:22:47 +0200

    Add local json and display game titles in plain text

---
+++ GameGrabber/Model/Game.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace GameGrabber.Model
+{
+    internal class Game
+    {
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+
+        [JsonProperty(PropertyName = "image")]
+        public string ImageUrl { get; set; }
+    }
+}
--- GameGrabber/ViewModel/OverviewVM.cs
+++ GameGrabber/ViewModel/OverviewVM.cs
@@ -1,6 +1,24 @@
-﻿namespace GameGrabber.ViewModel
+﻿using GameGrabber.Model;
+using GameGrabber.Repository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GameGrabber.ViewModel
 {
     internal class OverviewVM
     {
+        private IGameRepository _gameRepository = null;
+        public List<Game> Games { get; private set; }
+
+        public OverviewVM()
+        {
+            _gameRepository = new GameRepositoryLocal();
+            _ = GetGamesAsync();
+        }
+
+        public async Task GetGamesAsync()
+        {
+            Games = await _gameRepository.GetGamesAsync();
+        }
     }
 }
